badb1009359a4d8536231efa78c6cebc641ca1a7
Abid Ali KP <76676547+abidalikp@users.noreply.github.com>
2022-11-18 12:34:23 +0530

    Image url

--- mystery/settings.py
+++ mystery/settings.py
@@ -165,8 +165,8 @@ else:
     }
 
     AWS_LOCATION = 'static'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
     DEFAULT_FILE_STORAGE = 'mystery.storage_backends.MediaStorage'
 
